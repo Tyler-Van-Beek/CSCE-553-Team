4fa58e4808c50e7f670a1324db22e07532605bd8
Tyler-Van-Beek <C00157887@louisiana.edu>
2026-09-01 10:16:32 -0500

    PostgreSQL integration

--- lagniappe_signup/lagniappe_signup/settings.py
+++ lagniappe_signup/lagniappe_signup/settings.py
@@ -91,25 +91,25 @@ WSGI_APPLICATION = "lagniappe_signup.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 # PostgreSQL databse I was using. 
 
-# DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql_psycopg2",
-#        "NAME": 'lagniappe',
-#        "USER": 'postgres',
-#        'PASSWORD': 'Laval294!',
-#        'HOST': 'localhost',
-#        'PORT': '5432'
-#    }
-#}
+DATABASES = {
+   "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'lagniappe',
+        "USER": 'postgres',
+        'PASSWORD': 'Laval294!',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
